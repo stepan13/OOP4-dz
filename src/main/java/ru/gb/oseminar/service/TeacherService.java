111752cf8282c4de31f4dd7b038debd01a85c4ef
Vanger <stepan13@gmail.com>
2023-04-16 15:46:02 +0100

    fix

--- src/main/java/ru/gb/oseminar/service/TeacherService.java
+++ src/main/java/ru/gb/oseminar/service/TeacherService.java
@@ -1,6 +1,5 @@
 package ru.gb.oseminar.service;
 
-import ru.gb.oseminar.data.Student;
 import ru.gb.oseminar.data.Teacher;
 
 import java.time.LocalDate;
@@ -9,27 +8,27 @@ import java.util.List;
 
 public class TeacherService implements UserService<Teacher> {
 
-    private final List<Teacher> students;
+    private final List<Teacher> teachers;
 
     public TeacherService() {
-        this.students = new ArrayList<>();
+        this.teachers = new ArrayList<>();
     }
 
     @Override
     public List<Teacher> getAll() {
-        return students;
+        return teachers;
     }
 
     @Override
     public void create(String firstName, String secondName, String patronymic, LocalDate dateOfBirth) {
         Long countMaxId = 0L;
-        for (Teacher teacher: students){
+        for (Teacher teacher: teachers){
                 if (teacher.getTeacherId() > countMaxId){
                     countMaxId = teacher.getTeacherId();
             }
         }
         countMaxId++;
         Teacher teacher = new Teacher(firstName, secondName, patronymic, dateOfBirth, countMaxId);
-        students.add(teacher);
+        teachers.add(teacher);
     }
 }
